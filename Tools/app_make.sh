195fb6c00bac6ce46fa64ae5d45641a3ff125758
Bruno Levy <Bruno.Levy@inria.fr>
2023-07-18 08:50:13 +0200

    Geogram Android build tools work.

--- Tools/app_make.sh
+++ Tools/app_make.sh
@@ -1,4 +1,56 @@
 #!/bin/sh
-scriptdir=$(dirname "$0")
-geogramdir=$scriptdir/../../
+SCRIPT_DIR=$(dirname "$0")
+APP_MAIN_SRC=$1
+GEOGRAM_ANDROID_DIR="$SCRIPT_DIR/.."
+
+if [ ! -f $APP_MAIN_SRC ]; then
+    echo "Did not find main app source"
+    echo "usage: app_make.sh path_to_main.cpp"
+    exit
+fi
+
+APP_NAME=`dirname $APP_MAIN_SRC`
+APP_NAME=`basename $APP_NAME`
+
+APP_ID=$(echo $APP_NAME | tr A-Z a-z)
+APP_ID=com.inria.geogram.$APP_ID
+
+GEOGRAM_DIR=$(echo "$APP_MAIN_SRC" | sed -e 's|geogram/.*|geogram|')
+
+if [ ! -d $GEOGRAM_DIR ]; then
+    echo "Did not find GEOGRAM_DIR: $GEOGRAM_DIR"
+    exit
+fi
+
+APP_DIR=$GEOGRAM_ANDROID_DIR/Apps/$APP_NAME
+
+echo APP_MAIN_SRC=$APP_MAIN_SRC
+echo APP_NAME=$APP_NAME
+echo APP_ID=$APP_ID
+echo
+echo GEOGRAM_DIR=$GEOGRAM_DIR
+echo SCRIPT_DIR=$SCRIPT_DIR
+echo GEOGRAM_ANDROID_DIR=$GEOGRAM_ANDROID_DIR
+echo APP_DIR=$APP_DIR
+
+if [ -d $APP_DIR ]; then
+    echo "$APP_DIR already exists"
+    exit
+fi
+
+mkdir -p $APP_DIR
+cp -r $SCRIPT_DIR/AppSkeleton/* $APP_DIR
+for skel in `find $APP_DIR -name *.skel`
+do
+    cat $skel | sed \
+        -e "s|%APP_MAIN_SRC%|$APP_MAIN_SRC|g" \
+        -e "s|%APP_NAME%|$APP_NAME|g" \
+        -e "s|%APP_LONG_NAME%|$APP_NAME|g" \
+        -e "s|%APP_ID%|$APP_ID|g" \
+        -e "s|%GEOGRAM_DIR%|$GEOGRAM_DIR|g" \
+    > `dirname $skel`/`basename $skel .skel`
+    rm $skel                                                                
+done
+
+$SCRIPT_DIR/app_make_icons.sh $APP_DIR
 
--- Tools/app_make_icons.sh
+++ Tools/app_make_icons.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
-scriptdir=$(dirname "$0")
+SCRIPT_DIR=$(dirname "$0")
 
-if [ ! -d app/src/main/res ]; then
+APP_DIR=$1
+
+if [ ! -d $APP_DIR/app/src/main/res ]; then
    echo did not find app/src/main/res
    echo app_make_icons.sh should be run in app source directory
    exit
 fi
 
-if [ -z $1 ]; then
-   echo Missing label
+APP_NAME=`basename $1`
+APP_LABEL=$(echo $APP_NAME | cut -c 1 | tr a-z A-Z)
+echo APP_LABEL=$APP_LABEL
+
+#APP_CODE=`echo $APP_NAME | md5sum | sed -e 's| .*||g' | cut -c 1-8`
+#echo APP_CODE=$APP_CODE
+#APP_CODE=$(( 16#$APP_CODE % 100 ))
+#echo APP_CODE=$APP_CODE
+
+if [ ! -d $1 ]; then
+   echo Did not find app directory: $1
    exit
 fi
 
@@ -18,35 +29,35 @@ else
     HUE_CHANGE=$2
 fi
 
-mkdir -p app/src/main/res/mipmap-xxhdpi
-mkdir -p app/src/main/res/mipmap-xhdpi
-mkdir -p app/src/main/res/mipmap-hdpi
-mkdir -p app/src/main/res/mipmap-mdpi
+mkdir -p $APP_DIR/app/src/main/res/mipmap-xxhdpi
+mkdir -p $APP_DIR/app/src/main/res/mipmap-xhdpi
+mkdir -p $APP_DIR/app/src/main/res/mipmap-hdpi
+mkdir -p $APP_DIR/app/src/main/res/mipmap-mdpi
 
-convert ../tools/Generic/app/src/main/res/mipmap-xxhdpi/ic_launcher.png \
+convert $SCRIPT_DIR/geogram.png \
    -font DejaVu-Sans-Bold \
    -gravity center \
    -pointsize 70 \
-   -stroke  none      -fill white       -annotate 0 $1 \
-   -stroke  '#000094' -strokewidth 10   -annotate 0 $1 \
-   -stroke  '#000094' -strokewidth 2    -annotate 0 $1 \
-   $scriptdir/ic_launcher.png
+   -stroke  none      -fill white       -annotate 0 $APP_LABEL \
+   -stroke  '#000094' -strokewidth 10   -annotate 0 $APP_LABEL \
+   -stroke  '#000094' -strokewidth 2    -annotate 0 $APP_LABEL \
+   $APP_DIR/app/src/main/res/mipmap-xxhdpi/ic_launcher.png
 
 convert -modulate 100,100,$HUE_CHANGE \
-	app/src/main/res/mipmap-xxhdpi/ic_launcher.png \
-	app/src/main/res/mipmap-xxhdpi/ic_launcher.png
+	$APP_DIR/app/src/main/res/mipmap-xxhdpi/ic_launcher.png \
+	$APP_DIR/app/src/main/res/mipmap-xxhdpi/ic_launcher.png
 
-convert app/src/main/res/mipmap-xxhdpi/ic_launcher.png \
+convert $APP_DIR/app/src/main/res/mipmap-xxhdpi/ic_launcher.png \
         -geometry 48x48 \
-	app/src/main/res/mipmap-mdpi/ic_launcher.png
+	$APP_DIR/app/src/main/res/mipmap-mdpi/ic_launcher.png
 
-convert app/src/main/res/mipmap-xxhdpi/ic_launcher.png \
+convert $APP_DIR/app/src/main/res/mipmap-xxhdpi/ic_launcher.png \
         -geometry 72x72 \
-	app/src/main/res/mipmap-hdpi/ic_launcher.png
+	$APP_DIR/app/src/main/res/mipmap-hdpi/ic_launcher.png
 
-convert app/src/main/res/mipmap-xxhdpi/ic_launcher.png \
+convert $APP_DIR/app/src/main/res/mipmap-xxhdpi/ic_launcher.png \
         -geometry 96x96 \
-	app/src/main/res/mipmap-xhdpi/ic_launcher.png
+	$APP_DIR/app/src/main/res/mipmap-xhdpi/ic_launcher.png
 
 
-eog app/src/main/res/mipmap-xxhdpi/ic_launcher.png
+eog $APP_DIR/app/src/main/res/mipmap-xxhdpi/ic_launcher.png
