f9be14cdfb318531a42649f8588860c0f26e6dd0
Bruno Levy <Bruno.Levy@inria.fr>
2023-07-20 18:57:53 +0200

    Added command to install app and to display log

--- DOCS/NOTES.md
+++ DOCS/NOTES.md
@@ -21,7 +21,8 @@ What was blocking
   what we want to do is a good idea (`endless-tunnel` is a pure NDK app, without any Java)
 - `app/build.gradle`, in the second instance of `externalNativeBuild` (that is, *not* in `defaultConfig`),
   add `version "3.22.1"` right before `path`. Else it uses `CMake` from the NDK (version 3.18.1) that seems
-  to lack some features used by `geogram`
+  to lack some features used by `geogram`. Well in fact, not really a problem it seems, it is rather a
+  question of warnings.
 - make sure the shared object name in `app/src/main/AndroidManifest.xml` and the actual built shared object
   match
 - keep `android:exported="true"` in activity flags (still in `AndroidManifest.xml`). I set it to false, but
@@ -54,4 +55,4 @@ not exist in ImGui, to be checked.
 
 LINKS
 -----
--[link 1](https://gist.github.com/phlummox/24b488fa8656cf925014639bab9977e5)
+- [Android development command line cheatsheet](https://gist.github.com/phlummox/24b488fa8656cf925014639bab9977e5)
---
+++ Tools/app_install.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+APK=app/build/outputs/apk/debug/app-debug.apk
+
+help() {
+cat <<EOF
+NAME
+    app_install.sh
+
+SYNOPSIS
+    installs an android geomgram program
+
+USAGE
+    app_install.sh [options]
+
+OPTIONS
+
+    -h,-help,--help
+        Prints this page.
+
+    -release 
+        Installs a release app (default is debug)
+
+    -show-log
+        Clears and shows the Android log
+
+    -no-install
+        Does not install (to be used with -show-log)
+EOF
+}
+
+while [ -n "$1" ]; do
+    case "$1" in
+        -release)
+            RELEASE=1
+            shift
+            ;;
+        -show-log)
+            SHOW_LOG=1
+            shift
+            ;;
+        -no-install)
+            NO_INSTALL=1
+            shift
+            ;;
+        -h | -help | --help)
+            shift
+            help
+            exit
+            ;;
+        *)
+            echo "Error: unrecognized option: $1"
+            exit
+            ;;
+    esac
+done
+
+if [ -z $NO_INSTALL ]; then
+    if [ ! -z $RELEASE ]; then
+        APK=app/build/outputs/apk/release/app-release-signed.apk
+    fi
+
+    if [ ! -f $APK ]; then
+        echo "Did not find APK:$APK"
+        exit
+    fi
+
+    adb install -r $APK
+fi
+
+if [ ! -z $SHOW_LOG ]; then
+    echo "Displaying Android log (<ctrl><C> to exit)"
+    adb logcat -c
+    adb logcat | grep GEOGRAM
+fi
